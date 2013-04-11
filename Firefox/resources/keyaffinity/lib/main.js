29d2f260c89968dfa3bf3524ebe467c3a6314e20
kobitate94 <kobitate94@gmail.com>
2013-04-11 07:26:47 -0400

    Added version 1.3.1d

--- Firefox/resources/keyaffinity/lib/main.js
+++ Firefox/resources/keyaffinity/lib/main.js
@@ -3,6 +3,6 @@ var pageMod = require("page-mod");
 pageMod.PageMod({
   include: ["*.furaffinity.net"], 
   contentScriptWhen: 'ready',
-  contentScriptFile: [data.url("jquery.js"), data.url("hotkeys.js"), data.url("keyaffinity.js")],
+  contentScriptFile: [data.url("jquery-1.9.1.js"), data.url("hotkeys.js"), data.url("keyaffinity.js")],
   contentStyleFile: data.url("styles.css")
 });
